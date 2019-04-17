b31b336e00bbdfc69ad6e225f507a8007c7f4836
jama8050 <jama8050@colorado.edu>
2019-04-17 17:24:54 -0600

    Port stuff

--- liveChat/server.js
+++ liveChat/server.js
@@ -466,7 +466,8 @@ io.on('connection', function(socket){
 });
 
 // start server on port 8888
-var portPort = process.env.port || 8888;
+var portPort = (process.env.port) ? process.env.port : 8888;
+console.log(portPort);
 server.listen(portPort);
 /*console.log('http://localhost:8888 is the home page');
 console.log('http://localhost:8888/login is the login page');
