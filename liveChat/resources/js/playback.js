467cfeb5f6a150ef23492ca26444d17b61393baa
jama8050 <jama8050@colorado.edu>
2019-04-19 17:06:28 -0600

    Formatting changes

--- liveChat/resources/js/playback.js
+++ liveChat/resources/js/playback.js
@@ -3,10 +3,10 @@ var player;
 window.onSpotifyWebPlaybackSDKReady = () => {
     //token is used to connect with spotify connect leaving this undone not sure how we want to authorize this
     //have to have spotify premium and get a code which expires every hour
-    var token = 'BQCrgW0ek3cVbXp2iDOmvHWP0v76mcv6NA2TY9OpbpnWPyuoDPRoiQd7dp083UZX7IvGAulS7xvszGJUvj6kCyI6lvdmXvK24rznsih6jnjsgJzteppb6oXmWl2Le_msp_gXT_7Oe4LhX9VVIlqyroXlUZekXzPns3HZ';
+    var token = 'BQC841zk_y1N0Et3RprKWEsw2jykpRJjZYJi2aSNnpByFiS18MDHIp6dMVlEmgZH8iWhJEQSOt2rh1ePwn_aiP4c2Lf3HznxhC3lg_qRucLQ5vRayPUUIjMQRKb_YaN34NNjVmgcwhotjVjLTCBh4SqoQFunhgjVlZCMG3ns';
     player = new Spotify.Player({
     name: 'Tracked Out',
-    getOAuthToken: cb => { cb(token); }
+        getOAuthToken: cb => { cb(token); }
     });
     // Error handling
     player.addListener('initialization_error', ({ message }) => { console.error(message); });
@@ -17,27 +17,27 @@ window.onSpotifyWebPlaybackSDKReady = () => {
     // Playback status updates
     //player.addListener('player_state_changed', state => { console.log(state); });
     player.addListener('player_state_changed', ({
-      position,
-      duration,
-      track_window: { current_track }
+        position,
+        duration,
+        track_window: { current_track }
     }) => {
-      console.log('Currently Playing', current_track);
-      console.log('Position in Song', position);
-      console.log('Duration of Song', duration);
-      console.log("Album Image", current_track.album.images[2].url); //The album image url of the currently playing track
-      document.getElementById("song_title").innerHTML = current_track.name;//sets song name
-      document.getElementById("artist_name").innerHTML = current_track.artists[0].name;
-      document.getElementById("album").src = current_track.album.images[2].url;//sets the album image
+        console.log('Currently Playing', current_track);
+        console.log('Position in Song', position);
+        console.log('Duration of Song', duration);
+        console.log("Album Image", current_track.album.images[2].url); //The album image url of the currently playing track
+        $("#song_title").html(current_track.name);//sets song name
+        $("#artist_name").html(current_track.artists[0].name);
+        $("#album").attr('src', current_track.album.images[2].url);//sets the album image
     });
 
     // Ready
     player.addListener('ready', ({ device_id }) => {
-      console.log('Ready with Device ID', device_id);
+        console.log('Ready with Device ID', device_id);
     });
 
     // Not Ready
     player.addListener('not_ready', ({ device_id }) => {
-      console.log('Device ID has gone offline', device_id);
+        console.log('Device ID has gone offline', device_id);
     });
 
     // Connect to the player
@@ -49,15 +49,15 @@ window.onSpotifyWebPlaybackSDKReady = () => {
 var toggle = 1;
 
 function mute_button(){
-  if(toggle === 0){
-    player.setVolume(1).then(() => {
-      console.log('Volume');
-      toggle = 1;
-    });
-  }else if(toggle === 1){
-    player.setVolume(0).then(() => {
-      console.log('muted');
-      toggle = 0;
-    });
-  }
+    if(toggle === 0){
+        player.setVolume(1).then(() => {
+            console.log('Volume');
+            toggle = 1;
+        });
+    }else if(toggle === 1){
+        player.setVolume(0).then(() => {
+            console.log('muted');
+            toggle = 0;
+        });
+    }
 }
